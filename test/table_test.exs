e003ca1e8dd48437eb0a9d7a1eaa4ca7dd085af4
“nicolafiorillo” <nicola.fiorillo@gmail.com>
2020-02-25 10:54:07 +0100

    Test on projection.

--- test/table_test.exs
+++ test/table_test.exs
@@ -1,7 +1,7 @@
 defmodule TableTest do
   use ExUnit.Case
 
-  test "register a new user" do
+  test "register a new table" do
     Seven.EventStore.EventStore.subscribe("TableOpen", self())
 
     request_id = Seven.Data.Persistence.new_id
@@ -18,5 +18,27 @@ defmodule TableTest do
     refute result == :not_managed, "Command is not managed by anyone"
 
     assert_receive %Seven.Otters.Event{type: "TableOpen", request_id: ^request_id, correlation_module: Cafe.Aggregate.Table}
+  end
+
+  test "register a new table and check if it is in list" do
+    table_number = "43"
+
+    %Seven.CommandRequest{
+      id: Seven.Data.Persistence.new_id,
+      command: "OpenTable",
+      sender: __MODULE__,
+      params: %{number: table_number, waiter: "Bob"}
+    }
+    |> Seven.CommandBus.send_command_request()
+
+    Seven.Test.Helper.wait()
+
+    # method 1
+    {:ok, module} = Seven.Projections.get_projection("Tables")
+    tables = module.filter(fn c -> c.number == table_number end)
+    assert length(tables) == 1
+
+    #method 2
+    {:ok, [%Cafe.Projection.Tables{number: "43", waiter: "Bob"}]} = Cafe.Projection.Tables.query(:table, table_number)
   end
 end
